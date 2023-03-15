74afdd3dff47b0c854b39e57b4f6febb5510f108
ndah nerius <ndahnerius@gmail.com>
2023-03-15 06:17:52 +0100

    new commit

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+    var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for (tablink of tablinks){
+            tablink.classList.remove('active-link');
+        }
+        for (tabcontent of tabcontents){
+            tabcontent.classList.remove('active-tab');
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+       
+    }
 
 
+    window.addEventListener('scroll', function(){
+        const header = document.querySelector('nav');
+        header.classList.toggle('sticky', window.scrollY > 0);
+    });
+
+    function openmenu(){
+        const sidemenu = document.getElementById('sidemenu')
+        sidemenu.style.right = '0'
+    }
+    function closemenu(){
+        const sidemenu = document.getElementById('sidemenu')
+        sidemenu.style.right = '-200px'
+    }
